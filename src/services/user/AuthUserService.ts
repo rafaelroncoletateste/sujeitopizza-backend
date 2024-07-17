85a7c4bdf5d082af3829b1c50a87d5d4741528a0
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-07-17 10:23:35 -0300

    Gerando Token do Usuário

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -24,6 +25,25 @@ export class AuthUserService {
       throw new Error("Email/Senha incorreto");
     }
 
-    return { ok: true }
+    // Token JWT
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
+    // //
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
