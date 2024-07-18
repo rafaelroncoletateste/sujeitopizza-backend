d19c98c300c15047ece8a961cf9d1f5e8fc2f692
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-07-18 09:20:09 -0300

    Cadastrando Categorias

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,20 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+
 const router = Router();
 
 // === ROTAS USER ===
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// === ROTAS CATEGORY ===
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
