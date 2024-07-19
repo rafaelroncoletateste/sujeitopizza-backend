3cf5480c85c10c029e4e85337dc559183266553f
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-07-19 11:11:43 -0300

    Fechando pedido

--- src/controllers/product/ListByCategoryController.ts
+++ src/controllers/product/ListByCategoryController.ts
@@ -3,7 +3,7 @@ import { ListByCategoryService } from "../../services/product/ListByCategoryServ
 
 export class ListByCategoryController {
   async handle(req: Request, res: Response) {
-    const category_id = req.params.category_id;
+    const category_id = req.query.category_id as string;
 
     const listByCategoryService = new ListByCategoryService();
     const products = await listByCategoryService.execute({ category_id });
--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ import { CreateProductController } from "./controllers/product/CreateProductCont
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -48,5 +49,6 @@ router.get(
 
 // === ROTAS ORDER ===
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
+router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 
 export { router };
